d6dddbbe6251d76d3a30abbc525c0aa4e7c1af20
Naran Torguud <ntorguud@gmail.com>
2021-06-08 23:01:56 -0700

    Update pages

--- index.php
+++ index.php
@@ -14,12 +14,12 @@
         <h1>Popular Communities</h1>
             <div class="popcomms">
                 <h2>Emeral City Wanderers</h2>
-                <img src="images/wanderers.jpeg" alt="wanderers" width="70px">
+                <img src="images/wanderers.jpeg" alt="wanderers" width="70px" height="70px">
                 <p>This is a Seattle-based chapter of America’s Walking Club, and our club is known as the “Holiday Club” because we sponsor organized walking events on or near holidays – New Year’s Eve and Day, Easter, Fourth of July, Labor Day, and Halloween.   We also host many self-guided year-round-walks in Seattle.</p>
             </div>
             <div class="popcomms">
                 <h2>Seattle Singles</h2>
-                <img src="images/singles.jpeg" alt="singles" width="70px">
+                <img src="images/singles.jpeg" alt="singles" width="70px" height="70px">
                 <p>This is a singles social events page for singles who enjoy a more outgoing lifestyle through amazing, diverse and cultivated events located in Seattle, WA, and its surrounding areas. Come enjoy great theater, live music, fine dining, arts, outdoor adventures, sporting events, wine and cocktail parties and so much more! Come see what Seattle has to offer! Seattle Singles offers many fun and adventurous events and venues for singles of all ages.</p>
             </div>
         </div>
@@ -28,12 +28,12 @@
         <h1>New Communities</h1>
             <div class="newcomms">
                 <h2>The Seattle Vegan Meetup Group</h2>
-                <img src="images/vegangroup.jpeg" alt="vegan-group" width="70px">
+                <img src="images/vegangroup.jpeg" alt="vegan-group" width="70px" height="70px">
                 <p>This is an eclectic group of Seattle-area vegans and veg-curious. It's a lot of fun to meet other vegans and we welcome everyone interested in a vegan diet / lifestyle.</p>
             </div>
             <div class="newcomms">
                 <h2>Club Seattle Runners Division (CSRD)</h2>
-                <img src="images/runners.jpeg" alt="runners-club" width="70px">
+                <img src="images/runners.jpeg" alt="runners-club" width="70px" height="70px">
                 <p>This group meets every Thursday evening for a run at your own pace (all levels welcome!), followed by hangin’ at one of the many local breweries in the area.” They urge you to “come out and meet other members of our community who believe that no run is complete without a tasty pint (or 3!) to wash it down.</p>
             </div>
         </div>
