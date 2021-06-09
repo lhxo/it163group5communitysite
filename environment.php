b0b2e6062e9039b269bc9ad66972efb6f431a9c6
Naran Torguud <ntorguud@gmail.com>
2021-06-08 19:24:04 -0700

    Update page

---
+++ environment.php
@@ -0,0 +1,36 @@
+<?php include 'includes/header.php' ?>
+
+<div class="main"> 
+        <div class="about">
+            <h4>Environmental Activism</h4>
+                <img src="images/environment.jpeg" alt="environment"><br>
+                <ul>
+                    <li><a href="https://www.facebook.com/CCLSeattle/">Seattle Citizen's Climate Lobby (CCL) and CCL Bellevue</a>
+                        <p>CCL helps “build the political will to support national legislation to address climate change and accelerate the transition to a sustainable, clean energy-based economy.” They  “focus on developing relationships with congressional representatives and the local media through letters, phone calls, and meetings.” Events include monthly meetings, letter-writing, workshops and more.</p></li>
+                    <li><a href="https://350seattle.org/">350 Seattle</a>
+                        <p>This is a volunteer-powered climate justice organization organized by interest groups called “workgroups.” They make it easy to find ways to let your voice be heard on climate issues through events such as protests, public hearings, and blockades. 350 Seattle part of the national organization, 350.org.</p></li>
+                </ul>
+        </div>
+
+        <div class="categories">
+            <h1>Categories</h1>
+            <div class="cat">
+                <div class="types"><a href="networking.php">Professional Networking</a></div>
+                <div class="types"><a href="environment.php">Environmental Activism</a></div>
+                <div class="types"><a href="lifestyle.php">Sustainable Lifestyle</a></div>
+                <div class="types"><a href="homes.php">Sustainable Homes</a></div>
+                <div class="types"><a href="vegan.php">Vegan & vegetarian</a></div>
+                <div class="types"><a href="hiking.php">Hiking & outdoor exploration</a></div>
+                <div class="types"><a href="running.php">Running</a></div>
+                <div class="types"><a href="cycling.php">Cycling</a></div>
+                <div class="types"><a href="conversation.php">Conversation</a></div>
+                <div class="types"><a href="practice.php">Practice</a></div>
+                <div class="types"><a href="organization.php">Organization</a></div>
+                <div class="types"><a href="communities.php">12</a></div>
+            </div> <!-- end cat-->
+        </div> <!-- end categories-->
+        <br>
+        <br>
+    </div><!-- end main-->
+
+    <?php include 'includes/footer.php' ?>
