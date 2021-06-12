d954f6b8d62884140c7fc8ad56506fa00d436b3c
Naran Torguud <ntorguud@gmail.com>
2021-06-12 13:51:28 -0700

    Update pages

--- about.php
+++ about.php
@@ -1,5 +1,8 @@
 <?php include 'includes/header.php' ?>
 
+<body onload="startTime()">
+<div id="timer2"></div>
+
  	<div class="main">
         <div class="splash">
             <p>About Us</p>
@@ -20,38 +23,70 @@
             <img src="../images/IMG_1459.jpg" alt="">
             <h2>Julio Ramos</h2>
             <p>I'm a New Englander who moved to Seattle in 2019, right before the COVID-19 pandemic. I'm a grocery worker who is working to become a web designer. I'm focused on functional, beautiful, and user friendly websites. I love to read, watch old horror movies, and coding!</p>
-            <h3>Communities I Follow</h3>
-            <a href="https://www.meetup.com/Seattle-Greenlake-Running-Group/"><h2>Seattle Greenlake Running Group</h2></a>
-			<p>This group has extensive collection of weekly and biweekly running meetups at Green Lake and the surrounding neighborhoods. The Seattle Green Lake Running Group was created with the intent of creating a running community that supports and encourages a healthy and active lifestyle through group events and participation.</p>
-            <a href="https://www.meetup.com/Northwest-Woodworkers-Association/"><h2>Northwest Woodworkers Association</h2></a>
-			<p>The Northwest Woodworkers Association is a forum for woodworkers of all skill levels to share their common interests. Our members interests cover all aspects of woodworking including: carving, turning, furniture, cabinetry, marquetry, scroll work, and many others. The group meets monthly.</p>
-            <a href="https://www.meetup.com/Seattle-Fun-Adventure-Group/"><h2>Fun & Adventure Group</h2></a>
-			<p>Fun and Adventures is basically all about FUN i.e. F is for Friends who do stuff together, U is for you and me, N is for Anywhere, Anytime, Anything, Anyone :) Emerald City is very big and it is very tough to meet people. This group is designed to make it a whole lot easier in bringing people together for some fun activities.</p>
+            <h2>Communities I Follow</h2>
+            <div class="accordion">
+                <h3>Seattle Greenlake Running Group</h3>
+                <div>
+                    <a href="https://www.meetup.com/Seattle-Greenlake-Running-Group/">Seattle Greenlake Running Group</a>
+                    <p>This group has extensive collection of weekly and biweekly running meetups at Green Lake and the surrounding neighborhoods. The Seattle Green Lake Running Group was created with the intent of creating a running community that supports and encourages a healthy and active lifestyle through group events and participation.</p>
+                </div>
+                <h3>Northwest Woodworkers Association</h3>
+                <div>
+                    <a href="https://www.meetup.com/Northwest-Woodworkers-Association/">Northwest Woodworkers Association</a>
+                    <p>The Northwest Woodworkers Association is a forum for woodworkers of all skill levels to share their common interests. Our members interests cover all aspects of woodworking including: carving, turning, furniture, cabinetry, marquetry, scroll work, and many others. The group meets monthly.</p>
+                </div>
+                <h3>Fun & Adventure Group</h3>
+                <div>
+                    <a href="https://www.meetup.com/Seattle-Fun-Adventure-Group/">Fun & Adventure Group</a>
+                    <p>Fun and Adventures is basically all about FUN i.e. F is for Friends who do stuff together, U is for you and me, N is for Anywhere, Anytime, Anything, Anyone :) Emerald City is very big and it is very tough to meet people. This group is designed to make it a whole lot easier in bringing people together for some fun activities.</p>
+                </div>
+            </div> <!-- end accordion -->
         </div>
         <div class="third">
-            <p>Image Here</p>
+            <img src="../images/2019.jpg" alt="">
             <h2>Naran Torguud</h2>
-            <p>Description here</p>
-            <h3>Communities I Follow</h3>
-            <a href="https://www.meetup.com/Seattle-Language-Culture-Meetup/"><h2>Seattle Language & Culture</h2></a>
-			<p>This is a group for anyone interested in any language or world culture. Any level of experience with a language is welcome.</p>
-            <a href="https://www.meetup.com/SeattleSinglesItzaDate/"><h2>Seattle Singles</h2></a>
-			<p>This is a singles social events page for singles who enjoy a more outgoing lifestyle through amazing, diverse and cultivated events located in Seattle, WA, and its surrounding areas. Come enjoy great theater, live music, fine dining, arts, outdoor adventures, sporting events, wine and cocktail parties and so much more! Come see what Seattle has to offer! Seattle Singles offers many fun and adventurous events and venues for singles of all ages.</p>
-            <a href="http://www.emeraldcitywanderers.org/"><h2>Emerald City Wanderers</h2></a>
-			<p>This is a Seattle-based chapter of America’s Walking Club, and our club is known as the “Holiday Club” because we sponsor organized walking events on or near holidays – New Year’s Eve and Day, Easter, Fourth of July, Labor Day, and Halloween. We also host many self-guided year-round-walks in Seattle.</p>
+            <p>I moved to Seattle in 2019, before the COVID-19 as well as Julio. My background is knitting designer. I want to be a web-developer who create well-designed and functional websites. I like playing volleyball, traveling and hiking.</p>
+            <h2>Communities I Follow</h2>
+            <div class="accordion">
+                <h3>Seattle Language & Culture</h3>
+                <div>
+                    <a href="https://www.meetup.com/Seattle-Language-Culture-Meetup/">Seattle Language & Culture</a>
+                    <p>This is a group for anyone interested in any language or world culture. Any level of experience with a language is welcome.</p>
+                </div>
+                <h3>Seattle Singles</h3>
+                <div>
+                    <a href="https://www.meetup.com/SeattleSinglesItzaDate/">Seattle Singles</a>
+                    <p>This is a singles social events page for singles who enjoy a more outgoing lifestyle through amazing, diverse and cultivated events located in Seattle, WA, and its surrounding areas. Come enjoy great theater, live music, fine dining, arts, outdoor adventures, sporting events, wine and cocktail parties and so much more! Come see what Seattle has to offer! Seattle Singles offers many fun and adventurous events and venues for singles of all ages.</p>
+                </div>
+                <h3>Emerald City Wanderers</h3>
+                <div>
+                    <a href="http://www.emeraldcitywanderers.org/">Emerald City Wanderers</a>
+                    <p>This is a Seattle-based chapter of America’s Walking Club, and our club is known as the “Holiday Club” because we sponsor organized walking events on or near holidays – New Year’s Eve and Day, Easter, Fourth of July, Labor Day, and Halloween. We also host many self-guided year-round-walks in Seattle.</p>
+                </div>
+            </div> <!-- end accordion -->
         </div>
         <div class="third">
             <img src="../images/yip.jpg" alt="">
             <h2>Christopher Yip</h2>
             <p>I moved to Seattle in 2016 and used to work as a chef, but now I am working as a carpenter and studying to become a web developer. Within web development I am looking to create web applications to help connect people. I am into biking, board games, bouldering, and cooking.</p>
-            <h3>Communities I Follow</h3>
-            <a href="https://www.meetup.com/Seattle-Greenlake-Running-Group/"><h2>Seattle Greenlake Running Group</h2></a>
-			<p>This group has extensive collection of weekly and biweekly running meetups at Green Lake and the surrounding neighborhoods. The Seattle Green Lake Running Group was created with the intent of creating a running community that supports and encourages a healthy and active lifestyle through group events and participation.</p>
-            <a href="https://www.meetup.com/Northwest-Woodworkers-Association/"><h2>Northwest Woodworkers Association</h2></a>
-			<p>The Northwest Woodworkers Association is a forum for woodworkers of all skill levels to share their common interests. Our members interests cover all aspects of woodworking including: carving, turning, furniture, cabinetry, marquetry, scroll work, and many others. The group meets monthly.</p>
-            <a href="https://www.meetup.com/Seattle-Fun-Adventure-Group/"><h2>Fun & Adventure Group</h2></a>
-			<p>Fun and Adventures is basically all about FUN i.e. F is for Friends who do stuff together, U is for you and me, N is for Anywhere, Anytime, Anything, Anyone :) Emerald City is very big and it is very tough to meet people. This group is designed to make it a whole lot easier in bringing people together for some fun activities.</p>
-
+            <h2>Communities I Follow</h2>
+            <div class="accordion">
+                <h3>Seattle Greenlake Running Group</h3>
+                <div>
+                    <a href="https://www.meetup.com/Seattle-Greenlake-Running-Group/">Seattle Greenlake Running Group</a>
+                    <p>This group has extensive collection of weekly and biweekly running meetups at Green Lake and the surrounding neighborhoods. The Seattle Green Lake Running Group was created with the intent of creating a running community that supports and encourages a healthy and active lifestyle through group events and participation.</p>
+                </div>
+                <h3>Northwest Woodworkers Association</h3>
+                <div>
+                    <a href="https://www.meetup.com/Northwest-Woodworkers-Association/">Northwest Woodworkers Association</a>
+                    <p>The Northwest Woodworkers Association is a forum for woodworkers of all skill levels to share their common interests. Our members interests cover all aspects of woodworking including: carving, turning, furniture, cabinetry, marquetry, scroll work, and many others. The group meets monthly.</p>
+                </div>
+                <h3>Fun & Adventure Group</h3>
+                <div>
+                    <a href="https://www.meetup.com/Seattle-Fun-Adventure-Group/">Fun & Adventure Group</a>
+                    <p>Fun and Adventures is basically all about FUN i.e. F is for Friends who do stuff together, U is for you and me, N is for Anywhere, Anytime, Anything, Anyone :) Emerald City is very big and it is very tough to meet people. This group is designed to make it a whole lot easier in bringing people together for some fun activities.</p>
+                </div>
+            </div> <!-- end accordion -->
         </div>
     </div>
     
---
+++ js/test.js
@@ -0,0 +1,12 @@
+var myVar = setInterval(myTimer, 1000);
+
+function myTimer() {
+  var d = new Date();
+  document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+}
+
+var myVar = setInterval(myTimer, 1000);
+function myTimer() {
+  var d = new Date();
+  document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+} 
