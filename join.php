15985f2c23081e1bed8960baca00531f123b226f
Julio Ramos <64497770+j-ramos-css@users.noreply.github.com>
2021-06-14 14:05:11 -0700

    Login page updated

--- join.php
+++ join.php
@@ -1,57 +1,68 @@
 <?php include 'includes/header.php' ?>
 
-<div class="register-login">
-    <div class="register">
-      <h2>New Account</h2>
-      <!-- <form method="post" action="http://www.example.org/register"> -->
-      <form action="<?php echo basename($_SERVER['PHP_SELF']); ?>" method="post">
- 
-        <label for="username">Create a username: </label><br>
-        <input type="text" id="username"/><div id="feedback"></div>
- 
-        <label for="password">Create a password: </label><br>
-        <input type="password" id="password"/><div id="feedback"></div>
- 
-        <input type="submit" value="Sign up"/>
- 
-      </form>
-      </div>
+	<div class="register-login">
+		<div class="register">
+			<h2>New Account</h2>
+			<!-- <form method="post" action="http://www.example.org/register"> -->
+			<form action="<?php echo basename($_SERVER['PHP_SELF']); ?>" method="post">
 
-      <div class="login">
-      <h2>Login</h2>
-      <form method="post" action="http://www.example.org/login">
- 
-        <label for="username">Username: </label><br>
-        <input type="text" id="username"/><div id="feedback"></div>
- 
-        <label for="password">Password: </label><br>
-        <input type="password" id="password"/><div id="feedback"></div>
- 
-        <input type="submit" value="Login"/>
- 
-      </form>
-    </div>
+				<label for="email">Email: </label><br>
+				<input type="text" id="email" placeholder="Email"/>
+				<div id="feedback"></div>
+				<br>
+				<label for="username">Create a username: </label><br>
+				<input type="text" id="username" placeholder="Username"/>
+				<div id="feedback"></div>
+				<br>
+				<label for="password">Create a password: </label><br>
+				<input type="password" id="password" placeholder="Password"/>
+				<div id="feedback"></div>
+				<br>
+				<input type="submit" value="Sign up" class="login-button"/>
+				<br><br>
+				<div class="g-recaptcha" data-sitekey="<?=$siteKey;?>"></div>
 
-    <div class="g-recaptcha" data-sitekey="<?=$siteKey;?>"></div> 
-    </div> <!-- end register-login -->
+			</form>
+		</div>
 
-    <script>
-      function checkRecaptcha() {
-            res = $('#g-recaptcha-response').val();
-            if (res == "" || res == undefined || res.length == 0)
-                return false;
-            else
-                return true;
-        }
-        $(function() {
-            let thisForm = document.forms[0];
-            $(thisForm).submit(function(e) {            
-                if(!checkRecaptcha()) {
-                    alert("Please confirm you are not a robot.");
-                    return false;
-                }
-            });
-        }); 
-    </script>
+		<div class="login">
+			<h2>Login</h2>
+			<form method="post" action="http://www.example.org/login">
+
+				<label for="username">Username: </label><br>
+				<input type="text" id="username" />
+				<div id="feedback"></div>
+				<br>
+				<label for="password">Password: </label><br>
+				<input type="password" id="password" />
+				<div id="feedback"></div>
+				<a class="forgotpw" href="#">Forgot your password?</a>
+				<br><br>
+				<input type="submit" value="Login" class="login-button" />
+
+			</form>
+		</div>
+
+
+	</div> <!-- end register-login -->
+
+	<script>
+		function checkRecaptcha() {
+			res = $('#g-recaptcha-response').val();
+			if (res == "" || res == undefined || res.length == 0)
+				return false;
+			else
+				return true;
+		}
+		$(function() {
+			let thisForm = document.forms[0];
+			$(thisForm).submit(function(e) {
+				if (!checkRecaptcha()) {
+					alert("Please confirm you are not a robot.");
+					return false;
+				}
+			});
+		});
+	</script>
 
     <?php include 'includes/footer.php' ?>
